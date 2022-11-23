a579bb56e14c2e45f3042004d6ba5614bd8fd591
fgorkhs <fgorkhs@gmail.com>
2022-11-23 22:14:05 +0100

    Got data loaded

--- download_script.sh
+++ download_script.sh
@@ -1,4 +1,3 @@
 wget https://tierstatistik.identitas.ch/data/cattle-map-commune.csv
 wget https://data.geo.admin.ch/ch.swisstopo.swissboundaries3d/swissboundaries3d_2022-05/swissboundaries3d_2022-05_2056_5728.shp.zip
-wget 
-wget 
+# Unwgettable: https://www.bfs.admin.ch/bfs/en/home/statistics/population/births-deaths/first-names.assetdetail.23085973.html
