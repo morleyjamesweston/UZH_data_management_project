3c1959e80cb18ba8dd53fc55bd957efe1186f155
fgorkhs <fgorkhs@gmail.com>
2022-11-23 18:27:58 +0100

    Commit before pull

---
+++ download_script.sh
@@ -0,0 +1,4 @@
+wget https://tierstatistik.identitas.ch/data/cattle-map-commune.csv
+wget https://data.geo.admin.ch/ch.swisstopo.swissboundaries3d/swissboundaries3d_2022-05/swissboundaries3d_2022-05_2056_5728.shp.zip
+wget 
+wget 
